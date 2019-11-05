8ae5d2eee0d3e147b9f50b6b314d9c0dab0db0b1
김구현 <siva3020@naver.com>
2019-11-05 07:33:29 +0000

    Revert "Merge branch 'readme' into 'develop'"
    This reverts merge request !89

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,24 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import mutations from './mutations'
-import getters from './getters'
-import actions from './actions'
+import data from './modules/data'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    movieSearchList: [],
-    movieUserList: [],
-    userData: [],
-    selectedMovie: {
-      'Activated': null,
-      'ActivatedMovie': null,
-      'selectedInfo': 1,
-    },
-    userData: [],
+  modules: {
+    data,
   },
-  mutations,
-  getters,
-  actions
 })
